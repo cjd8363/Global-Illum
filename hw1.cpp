cbcdf5a805d5dffcf0c4a03377c97f89b005bfe5
Charlene DiMeglio <cjd8363@g.rit.edu>
2014-02-12 14:39:32 -0500

    HW1

---
+++ hw1.cpp
@@ -0,0 +1,32 @@
+/* 
+ * Basic Ray Tracer Component for Hw1
+ * @author Charlene DiMeglio
+ * @author Jorge Leon
+ */
+
+#include string
+#include iostream
+ 
+public class RayTracer
+{
+    
+    private float height;
+    private float width;
+    private Color[][] pixels;
+    
+    // Takes in the height and width of the scene
+    public RayTracer(float height, float width)
+    {
+        this.height = height;
+        this.width = width;
+    }
+    
+    void main(int argc, char** argv)
+    {
+        
+    }
+};
+ 
+ 
+ 
+

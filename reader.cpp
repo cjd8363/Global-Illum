e38950c86a66a3534756531448ad7a8e03f212a2
jorgeleong <jorge.a.leon.g@gmail.com>
2014-02-19 15:38:29 -0500

    Reader files added
    txt file reader

---
+++ reader.cpp
@@ -0,0 +1,49 @@
+#include "reader.h"
+
+/*
+
+*/
+std::vector<std::string> getInfo (char* aloc){
+	std::vector<std::string> infoStruct;
+	std::ifstream fileOpen;
+	char buffer[40];
+	fileOpen.open(aloc);
+	
+	// Exception check
+	if (fileOpen.fail()){
+		std::cout << "file does not exist"<<std::endl;
+	}
+	
+	while(!fileOpen.eof()){
+		fileOpen.getline(buffer,40);
+		infoStruct.push_back(buffer);
+	}
+	return infoStruct;
+}
+
+/*
+
+*/
+float StringToNumber (const std::string &Text){
+	std::stringstream ss(Text);
+	float result;
+	return ss >> result ? result : 0;
+}
+
+/*
+
+*/
+std::vector<float> dimensions (std::string &sample){
+	std::string delim = ",";
+	size_t pos = 0;
+	std::vector<float> result;
+	
+	while ((pos = sample.find(delim)) != (std::string::npos)){
+		std::string token = sample.substr(0,pos);
+		result.push_back(StringToNumber(token));
+		sample.erase(0, pos + delim.length());
+	} 
+
+	result.push_back(StringToNumber(sample));
+	return result;
+}
---
+++ reader.h
@@ -0,0 +1,26 @@
+#ifndef _READER_H
+#define _READER_H
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <cstring>
+#include <sstream>
+
+/*
+	Extract strings from text file specified by char* location
+*/
+std::vector<std::string> getInfo (char* aloc);
+
+/*
+	Function to convert string into float
+*/
+float StringToNumber (const std::string &Text);
+
+/*
+	dimensions: outputs vector of float, from sample input
+	converts strings into float.
+*/
+std::vector<float> dimensions (std::string &sample);
+
+#endif
